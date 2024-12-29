6b23822b46e12cc9c01fad56a7a52c829c45de04
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2024-12-30 01:23:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+let fname = document.getElementById('fname');
+let lname = document.getElementById('lname');
+let phone = document.getElementById('phone');
+let mail = document.getElementById('mail');
+
+function  helper() {
+	alert(`First Name: ${fname.value} Last Name: ${lname.value} Phone Number: ${phone.value} Email ID:${mail.value}`)
+}
